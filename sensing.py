9a12a42751c8cd8d7d7f3116c09120afee78af17
sdorkenw <sven.dorkenwald@googlemail.com>
2017-02-06 21:31:58 +0100

    tested, fixed bugs and added command line call

--- sensing.py
+++ sensing.py
@@ -18,6 +18,8 @@ def sense(img, k=1000, basis="wvt", wvt_level=4, alpha=None):
         image
     :param k: int
         number of measurements
+    :param basis: str
+        either 'wvt', 'dct'
     :param wvt_level: int
         level of wavelet transform
     :param alpha: float or None
@@ -27,19 +29,19 @@ def sense(img, k=1000, basis="wvt", wvt_level=4, alpha=None):
         sensed image
     """
     img_f = img.flatten()
-    print("Build sensing matrix")
+    print "Build sensing matrix"
     A = np.random.normal(0, 1, len(img_f)*k).astype(np.float32).\
         reshape(k, img.shape[0], img.shape[1])
 
-    print("Measurement")
+    print "Measurement"
     b = np.dot(A.reshape(k, len(img_f)), img_f)
 
     if basis == "wvt":
-        print("Wavelets")
+        print "Wavelets"
         trans_A = [utils.dwt2(A[i].reshape(img.shape), level=wvt_level).
                      astype(np.float16).flatten() for i in range(k)]
     elif basis == "dct":
-        print("DCT")
+        print "DCT"
         trans_A = [utils.dct2(A[i].reshape(img.shape)).
                        astype(np.float16).flatten() for i in range(k)]
     else:
@@ -49,17 +51,17 @@ def sense(img, k=1000, basis="wvt", wvt_level=4, alpha=None):
 
     if alpha:
         lasso = lm.Lasso(alpha=alpha, max_iter=100000, normalize=True)
-        print("Fit")
+        print "Fit"
         lasso.fit(trans_A, b)
     else:
         lasso_cv = lm.LassoCV(n_jobs=cpu_count(), max_iter=100000,
                               normalize=True)
-        print("Fit")
+        print "Fit"
         lasso_cv.fit(trans_A, b)
-        print("Alpha: %.6f" % lasso_cv.alpha_)
+        print "Alpha: %.6f" % lasso_cv.alpha_
         lasso = lm.Lasso(alpha=lasso_cv.alpha_, max_iter=100000,
                          normalize=True)
-        print("Fit")
+        print "Fit"
         lasso.fit(trans_A, b)
 
     if basis == "wvt":
@@ -71,7 +73,7 @@ def sense(img, k=1000, basis="wvt", wvt_level=4, alpha=None):
 
 
 def sense_main(img_path, zoom_rate=1., k=1000, alpha=None, wvt_level=5,
-         save_path=None):
+               basis="wvt", save_path=None):
     """ Senses an image with k measurements and performs assisting work
 
     :param img_path: str
@@ -85,6 +87,8 @@ def sense_main(img_path, zoom_rate=1., k=1000, alpha=None, wvt_level=5,
         if None: alpha is first found with CV (takes long)
     :param wvt_level: int
         level of wavelet transform
+    :param basis: str
+        either 'wvt', 'dct'
     :param save_path: str
         path to folder to which images will be exported
         if ends with .png: only recovered image and cropped image are stored
@@ -94,9 +98,9 @@ def sense_main(img_path, zoom_rate=1., k=1000, alpha=None, wvt_level=5,
     img = imread(img_path)
 
     if len(img.shape) > 2:
-        print("Only grayscale supported...")
+        print "Only grayscale supported..."
         img = np.mean(img, axis=-1)
-        print("... RGB converted to grayscale")
+        print "... RGB converted to grayscale"
 
     if zoom_rate != 1:
         img = scipy.ndimage.zoom(img, zoom=zoom_rate, order=3)
@@ -107,15 +111,16 @@ def sense_main(img_path, zoom_rate=1., k=1000, alpha=None, wvt_level=5,
     img = img[int(np.floor(off[0])): img.shape[0] - int(np.ceil(off[0])),
               int(np.floor(off[1])): img.shape[1] - int(np.ceil(off[1]))]
 
-    print("Cropped image to: ", img.shape)
+    print "Cropped image to: ", img.shape
 
     img = img.astype(np.float16) / np.max(img)
 
-    r_img = sense(img.copy(), k=k, alpha=alpha, wvt_level=wvt_level)
+    r_img = sense(img.copy(), k=k, alpha=alpha, wvt_level=wvt_level,
+                  basis=basis)
     r_img[r_img < 0] = 0
 
-    print("rmse: %.3f" % (utils.compute_rmse(img, r_img)))
-    print("Sensing rate: %.3f" % (float(k) / np.product(img.shape)))
+    print "rmse: %.3f" % (utils.compute_rmse(img, r_img))
+    print "Sensing rate: %.3f" % (float(k) / np.product(img.shape))
 
     if save_path:
         if save_path.endswith(".png"):
@@ -140,10 +145,11 @@ def sense_main(img_path, zoom_rate=1., k=1000, alpha=None, wvt_level=5,
 
 def _sense_thread(args):
     sense_main(img_path=args[0], k=args[1], alpha=args[2], wvt_level=args[3],
-               save_path=args[4])
+               basis=args[4], save_path=args[5])
 
 
-def sense_multiple(ks, alphas, folder, img_path, wvt_level=4, n_processes=None):
+def sense_multiple(ks, alphas, folder, img_path, wvt_level=4, basis="wvt",
+                   n_processes=None):
     """ Senses multiple images with k(s) measuerements and alpha(s)
 
     :param ks: list of int
@@ -156,6 +162,8 @@ def sense_multiple(ks, alphas, folder, img_path, wvt_level=4, n_processes=None):
         path to image
     :param wvt_level: int
         level of wavelet transform
+    :param basis: str
+        either 'wvt', 'dct'
     :param n_processes:int or None
         number of processes to be used
         if None: all cores are used
@@ -173,7 +181,7 @@ def sense_multiple(ks, alphas, folder, img_path, wvt_level=4, n_processes=None):
         for a in alphas:
             params.append([img_path, k, a, wvt_level, folder])
 
-    print("N jobs: %d" % (len(params)))
+    print "N jobs: %d" % (len(params))
     if n_processes > 1:
         pool = Pool(n_processes)
         pool.map(_sense_thread, params)
@@ -184,14 +192,21 @@ def sense_multiple(ks, alphas, folder, img_path, wvt_level=4, n_processes=None):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage for parameter swiping: python2 sense.py <path_to_img> "
-              "<path_to_save_folder>")
+    if len(sys.argv) not in [3, 4]:
+        print "Usage for parameter swiping: python2 sense.py <path_to_img> " \
+              "<path_to_save_folder> [<basis>]"
+    else:
+        img_path = sys.argv[1]
+        folder = sys.argv[2]
 
-    img_path = sys.argv[1]
-    folder = sys.argv[2]
+        if len(sys.argv) == 4:
+            basis = sys.argv[3]
+            assert basis in ["wvt", "dct"]
+        else:
+            basis = "wvt"
 
-    ks = [8000, 16000, 2400]
-    alphas = np.logspace(-7, 0, num=20)
-    sense_multiple(ks, alphas, folder=folder, img_path=img_path)
-    utils.compute_rmse_folder(folder)
+        ks = [8000, 16000, 24000]
+        alphas = np.logspace(-7, 0, num=8)
+        sense_multiple(ks, alphas, folder=folder, img_path=img_path,
+                       basis=basis)
+        utils.compute_rmse_folder(folder)
